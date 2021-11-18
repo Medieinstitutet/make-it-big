e22c048fd7de91a428afbea3662d60ad5ec8ee5c
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2021-11-18 04:05:09 +0100

    add placeholder to input and change name regex

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,16 @@ const form = document.querySelector('form');
 const stringToUpperCase = str => str.toUpperCase();
 
 const renderFeedbackMessage = status => {
-  let template;
-  status === 'success'
-    ? (template = '<p class="success">This is your name in all uppercase</p>')
-    : (template = '<p class="error">Enter a valid name!</p>');
+  let template =
+    status === 'success'
+      ? '<p class="success">This is your name in all uppercase</p>'
+      : '<p class="error">Enter a valid name!</p>';
   document.body.lastElementChild.remove();
   form.insertAdjacentHTML('afterend', template);
 };
 
 const validateInput = userInput => {
-  const nameRegex = /^[a-zA-Z\s]*$/;
+  const nameRegex = /^[a-zA-Z]{2,}$/;
   if (nameRegex.test(userInput)) {
     form.username.value = stringToUpperCase(userInput);
     renderFeedbackMessage('success');
