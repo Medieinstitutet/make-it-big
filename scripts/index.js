6d0027e84b04248b2175ff1c0128c5d9389588cf
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2021-11-18 04:35:43 +0100

    add new name regex

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ const form = document.querySelector('form');
 const stringToUpperCase = str => str.toUpperCase();
 
 const renderFeedbackMessage = status => {
-  let template =
+  const template =
     status === 'success'
       ? '<p class="success">This is your name in all uppercase</p>'
       : '<p class="error">Enter a valid name!</p>';
@@ -12,7 +12,7 @@ const renderFeedbackMessage = status => {
 };
 
 const validateInput = userInput => {
-  const nameRegex = /^[a-zA-Z]{2,}$/;
+  const nameRegex = /^[\w\s]+$/;
   if (nameRegex.test(userInput)) {
     form.username.value = stringToUpperCase(userInput);
     renderFeedbackMessage('success');
